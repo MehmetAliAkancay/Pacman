c6da2de08f94a70974e0e9543b2e3431719fb4eb
Mehmet Ali Akançay <84207797+MehmetAliAkancay@users.noreply.github.com>
2023-05-23 12:48:44 +0300

    feat: add pacman
    Pacman'in yarısı bitmiş kısmı yüklendi.

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public Movement movement { get; private set; }
+    public GhostHome home { get; private set; }
+    public GhostScatter scatter { get; private set; }
+    public GhostChase chase { get; private set; }
+    public GhostFrightened frightened { get; private set; }
+    public GhostBehavior initialBehavior;
+    public Transform target;
+    public int points = 200;
+
+    private void Awake() {
+        this.movement = GetComponent<Movement>();
+        this.home = GetComponent<GhostHome>();
+        this.scatter = GetComponent<GhostScatter>();
+        this.chase = GetComponent<GhostChase>();
+        this.frightened = GetComponent<GhostFrightened>();
+    }
+
+    private void Start() {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        this.gameObject.SetActive(true);
+        this.movement.ResetState();
+
+        this.frightened.Disable();
+        this.chase.Disable();
+        this.scatter.Enable();
+    }
+}
---
+++ Assets/Scripts/Pacman.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Pacman : MonoBehaviour
+{
+    public Movement movement { get; private set;}
+
+    private void Awake() {
+        this.movement = GetComponent<Movement>();
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.W)){
+            this.movement.SetDirection(Vector2.up);
+        }
+        else if(Input.GetKeyDown(KeyCode.S)){
+            this.movement.SetDirection(Vector2.down);
+        }
+        else if(Input.GetKeyDown(KeyCode.A)){
+            this.movement.SetDirection(Vector2.left);
+        }
+        else if(Input.GetKeyDown(KeyCode.D)){
+            this.movement.SetDirection(Vector2.right);
+        }
+
+        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+    }
+
+    public void ResetState()
+    {
+        this.movement.ResetState();
+        this.gameObject.SetActive(true);
+    }
+}
